e0ac4b883c32ccd01078c333b5664d2668e002e9
Jeff McFadden <jeff@mcfadden.io>
2018-08-20 16:03:13 -0700

    Use base layout when installed.

--- app/controllers/oak/posts_controller.rb
+++ app/controllers/oak/posts_controller.rb
@@ -3,6 +3,8 @@ require_dependency "oak/application_controller"
 module Oak
   class PostsController < ApplicationController
     
+    layout '/application'
+    
     def index
       @posts = Post.live.order( published_at: :desc ).page(params[:page]).per(params[:per])
     end
