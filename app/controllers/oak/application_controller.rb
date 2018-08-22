925bbb5eea6389f30bb36c82e8fac06b7b9a0f50
Jeff McFadden <jeff@mcfadden.io>
2018-08-22 16:46:45 -0700

    Attempt to force timezone as a test.

--- app/controllers/oak/application_controller.rb
+++ app/controllers/oak/application_controller.rb
@@ -1,5 +1,12 @@
 module Oak
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    
+    before_action :set_time_zone
+    
+    def set_time_zone
+      Time.zone = 'America/Phoenix'
+    end
+    
   end
 end
