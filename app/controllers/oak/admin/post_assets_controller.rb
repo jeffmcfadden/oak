cdaede2f399cbfc92e7a3ec501f0f318221e553d
Jeff McFadden <jeff@mcfadden.io>
2018-08-24 15:02:16 -0700

    Add post asset index and destruction.

--- app/controllers/oak/admin/post_assets_controller.rb
+++ app/controllers/oak/admin/post_assets_controller.rb
@@ -3,9 +3,21 @@ require_dependency "oak/application_controller"
 module Oak
   class Admin::PostAssetsController < ApplicationController
     
+    def index
+      @post_assets = PostAsset.all.order( created_at: :desc ).page( params[:page] )
+    end
+    
     def create
       @post_asset = PostAsset.create post_asset_params
     end
+    
+    def destroy
+      @post_asset = PostAsset.find params[:id]
+      @post_asset.destroy
+      
+      redirect_to [:admin, :post_assets]
+    end
+    
     
     private
     
