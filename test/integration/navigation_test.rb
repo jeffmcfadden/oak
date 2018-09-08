11d13b3252911c64d28154f81541584187455b25
Jeff McFadden <jeff@mcfadden.io>
2018-09-08 09:57:01 -0700

    Get test working with actual route name.

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -2,10 +2,14 @@ require 'test_helper'
 
 class NavigationTest < ActionDispatch::IntegrationTest
 
+  include Oak::Engine.routes.url_helpers
+
+  setup do
+    @routes = Oak::Engine.routes
+  end
+
   test "The public posts page can load" do
-    puts "I'm here!"
-    
-    get '/posts'
+    get posts_path
     assert_equal 200, status    
   end
 
